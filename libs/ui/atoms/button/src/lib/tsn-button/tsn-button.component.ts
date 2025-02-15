b6a335b9f1d7a7613637ed28d39a26d580ac34a5
Pavel Suk <pavelsuk98@gmail.com>
2025-02-15 16:08:32 +0100

    feat: adjusting names

--- libs/ui/atoms/button/src/lib/tsn-button/tsn-button.component.ts
+++ libs/ui/atoms/button/src/lib/tsn-button/tsn-button.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'tsn-tsn-button',
+  selector: 'tsn-button',
   imports: [CommonModule],
   templateUrl: './tsn-button.component.html',
   styleUrl: './tsn-button.component.scss',
 })
-export class TsnButtonComponent {}
+export class TsnButton {}
--- libs/ui/atoms/icon/src/lib/tsn-icon/tsn-icon.component.ts
+++ libs/ui/atoms/icon/src/lib/tsn-icon/tsn-icon.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'tsn-tsn-icon',
+  selector: 'tsn-icon',
   imports: [CommonModule],
   templateUrl: './tsn-icon.component.html',
   styleUrl: './tsn-icon.component.scss',
 })
-export class TsnIconComponent {}
+export class TsnIcon {}
